4dbff323d091cffd2ca699ff93ed4bc1138e2d15
Maximilien Le Cleï <github.laborer138@passmail.net>
2025-12-16 19:27:35 +0000

    fixed wandb image logging

--- projects/classify_mnist/litmodule.py
+++ projects/classify_mnist/litmodule.py
@@ -1,3 +1,4 @@
+import torch
 import wandb
 
 from common.dl.litmodule.classification import BaseClassificationLitModule
@@ -7,4 +8,10 @@ class MNISTClassificationLitModule(BaseClassificationLitModule):
 
     @property
     def wandb_media_x(self):
-        return wandb.Image
+        def convert(x):
+            # Unnormalize: original = (normalized * std) + mean
+            x = x * 0.3081 + 0.1307
+            # Convert to 0-255 uint8
+            x = (x.clamp(0, 1) * 255).to(torch.uint8)
+            return wandb.Image(x)
+        return convert
