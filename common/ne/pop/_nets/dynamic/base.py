8e385b9447585cbb678a169f40ecdd6aec116453
Maximilien Le Cleï <github.laborer138@passmail.net>
2025-12-17 18:26:41 +0000

    bit of dynamic net restructuring

--- common/ne/pop/_nets/dynamic/base.py
+++ common/ne/pop/_nets/dynamic/base.py
@@ -21,16 +21,109 @@ log = logging.getLogger(__name__)
 
 
 class DynamicNets(BaseNets):
-
     def __init__(self, config: BaseNetsConfig):
         self.config = config
         self.nets: list[Net] = [
             Net(config.num_inputs, config.num_outputs)
             for _ in range(config.num_nets)
         ]
-        self._build_computation_tensors()
+        self.have_mutated = False
 
-    def _build_computation_tensors(self) -> None:
+    def mutate(self) -> None:
+        """Mutate all networks in the population."""
+        if self.have_mutated:
+            self.update_nets_standardization_values()
+        for net in self.nets:
+            net.mutate()
+        self.have_mutated = True
+        self.prepare_for_computation()
+
+    def __call__(self, x: Float[Tensor, "NN NI"]) -> Float[Tensor, "NN NO"]:
+        """Forward pass through the network population."""
+
+        log.debug("13. obs")
+        log.debug(x)
+        log.debug(x.shape)
+
+        flat_obs: Float[Tensor, "NNxNI"] = x.flatten()
+        log.debug("14. flat_obs")
+        log.debug(flat_obs)
+        log.debug(flat_obs.shape)
+
+        out: Float[Tensor, "TNNplus1"] = self.wrs.n_mean_m2_x_z[
+            :, 4
+        ].clone()  # z-score
+        out[self.input_nodes_indices] = flat_obs
+        log.debug("15. out")
+        log.debug(out)
+        log.debug(out.shape)
+
+        out: Float[Tensor, "TNNplus1"] = self.wrs(out)
+        log.debug("16. out")
+        log.debug(out)
+        log.debug(out.shape)
+
+        for j in range(self.max_num_network_passes_per_input):
+
+            log.debug(f"Pass {j}")
+            log.debug("17. num_network_passes_per_input_mask[j]")
+            log.debug(self.num_network_passes_per_input_mask[j])
+
+            mapped_out: Float[Tensor, "TNMN 3"] = torch.gather(
+                out, 0, self.flat_in_nodes_indices
+            ).reshape(-1, 3)
+            log.debug("18. mapped_out")
+            log.debug(mapped_out)
+            log.debug(mapped_out.shape)
+
+            matmuld_mapped_out: Float[Tensor, "TNMN"] = (
+                mapped_out * self.weights
+            ).sum(dim=1)
+            log.debug("19. matmuld_mapped_out")
+            log.debug(matmuld_mapped_out)
+            log.debug(matmuld_mapped_out.shape)
+            log.debug("19a. out[mutable_nodes_indices]")
+            log.debug(out[self.mutable_nodes_indices])
+            log.debug(out[self.mutable_nodes_indices].shape)
+            log.debug("19b. num_network_passes_per_input_mask[j]")
+            log.debug(self.num_network_passes_per_input_mask[j])
+            log.debug(self.num_network_passes_per_input_mask[j].shape)
+
+            out[self.mutable_nodes_indices] = torch.where(
+                self.num_network_passes_per_input_mask[j],
+                matmuld_mapped_out,
+                out[self.mutable_nodes_indices],
+            )
+            log.debug("20. out")
+            log.debug(out)
+            log.debug(out.shape)
+
+            out: Float[Tensor, "TNNplus1"] = self.wrs(out)
+            log.debug("21. out")
+            log.debug(out)
+            log.debug(out.shape)
+
+        output: Float[Tensor, "NN NO"] = out[
+            self.output_nodes_indices
+        ].reshape(self.config.num_nets, self.config.num_outputs)
+        log.debug("22. output")
+        log.debug(output)
+        log.debug(output.shape)
+
+        return output
+
+    def update_nets_standardization_values(self) -> None:
+        """Update each net's n_mean_m2_x_z from the batched tensor."""
+        for i in range(self.config.num_nets):
+            start = self.input_nodes_start_indices[i]
+            end = (
+                None
+                if i + 1 == self.config.num_nets
+                else self.input_nodes_start_indices[i + 1]
+            )
+            self.nets[i].n_mean_m2_x_z = self.wrs.n_mean_m2_x_z[start:end]
+
+    def prepare_for_computation(self) -> None:
         """Build all tensors needed for batched computation."""
         nets_num_nodes: Int[Tensor, "NN"] = torch.tensor(
             [len(net.nodes.all) for net in self.nets]
@@ -136,6 +229,7 @@ class DynamicNets(BaseNets):
         )
         log.debug("11. num_network_passes_per_input")
         log.debug(num_network_passes_per_input)
+
         for i in range(self.max_num_network_passes_per_input):
             for j in range(self.config.num_nets):
                 if self.nets[j].num_network_passes_per_input > i:
@@ -145,99 +239,6 @@ class DynamicNets(BaseNets):
         self.num_network_passes_per_input_mask = (
             self.num_network_passes_per_input_mask.bool()
         )
-
         log.debug("12. num_network_passes_per_input_mask")
         log.debug(self.num_network_passes_per_input_mask)
         log.debug(self.num_network_passes_per_input_mask.shape)
-
-    def __call__(self, x: Float[Tensor, "NN NI"]) -> Float[Tensor, "NN NO"]:
-        """Forward pass through the network population."""
-
-        log.debug("13. obs")
-        log.debug(x)
-        log.debug(x.shape)
-
-        flat_obs: Float[Tensor, "NNxNI"] = x.flatten()
-        log.debug("14. flat_obs")
-        log.debug(flat_obs)
-        log.debug(flat_obs.shape)
-
-        out: Float[Tensor, "TNNplus1"] = self.wrs.n_mean_m2_x_z[
-            :, 4
-        ].clone()  # z-score
-        out[self.input_nodes_indices] = flat_obs
-        log.debug("15. out")
-        log.debug(out)
-        log.debug(out.shape)
-
-        out: Float[Tensor, "TNNplus1"] = self.wrs(out)
-        log.debug("16. out")
-        log.debug(out)
-        log.debug(out.shape)
-
-        for j in range(self.max_num_network_passes_per_input):
-
-            log.debug(f"Pass {j}")
-            log.debug("17. num_network_passes_per_input_mask[j]")
-            log.debug(self.num_network_passes_per_input_mask[j])
-
-            mapped_out: Float[Tensor, "TNMN 3"] = torch.gather(
-                out, 0, self.flat_in_nodes_indices
-            ).reshape(-1, 3)
-            log.debug("18. mapped_out")
-            log.debug(mapped_out)
-            log.debug(mapped_out.shape)
-
-            matmuld_mapped_out: Float[Tensor, "TNMN"] = (
-                mapped_out * self.weights
-            ).sum(dim=1)
-            log.debug("19. matmuld_mapped_out")
-            log.debug(matmuld_mapped_out)
-            log.debug(matmuld_mapped_out.shape)
-            log.debug("19a. out[mutable_nodes_indices]")
-            log.debug(out[self.mutable_nodes_indices])
-            log.debug(out[self.mutable_nodes_indices].shape)
-            log.debug("19b. num_network_passes_per_input_mask[j]")
-            log.debug(self.num_network_passes_per_input_mask[j])
-            log.debug(self.num_network_passes_per_input_mask[j].shape)
-
-            out[self.mutable_nodes_indices] = torch.where(
-                self.num_network_passes_per_input_mask[j],
-                matmuld_mapped_out,
-                out[self.mutable_nodes_indices],
-            )
-            log.debug("20. out")
-            log.debug(out)
-            log.debug(out.shape)
-
-            out: Float[Tensor, "TNNplus1"] = self.wrs(out)
-            log.debug("21. out")
-            log.debug(out)
-            log.debug(out.shape)
-
-        output: Float[Tensor, "NN NO"] = out[
-            self.output_nodes_indices
-        ].reshape(self.config.num_nets, self.config.num_outputs)
-        log.debug("22. output")
-        log.debug(output)
-        log.debug(output.shape)
-
-        return output
-
-    def mutate(self) -> None:
-        """Mutate all networks in the population."""
-        self._sync_state_to_nets()
-        for net in self.nets:
-            net.mutate()
-        self._build_computation_tensors()
-
-    def _sync_state_to_nets(self) -> None:
-        """Update each net's n_mean_m2_x_z from the batched tensor."""
-        for i in range(self.config.num_nets):
-            start = self.input_nodes_start_indices[i]
-            end = (
-                None
-                if i + 1 == self.config.num_nets
-                else self.input_nodes_start_indices[i + 1]
-            )
-            self.nets[i].n_mean_m2_x_z = self.wrs.n_mean_m2_x_z[start:end]
