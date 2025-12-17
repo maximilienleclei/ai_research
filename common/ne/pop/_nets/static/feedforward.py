0f99436c39c2ad60b777f9b2042368c576cc6f9f
Maximilien Le Cleï <github.laborer138@passmail.net>
2025-12-17 16:56:28 +0000

    dim -> size static nets

--- common/ne/pop/_nets/static/feedforward.py
+++ common/ne/pop/_nets/static/feedforward.py
@@ -2,10 +2,10 @@
 
 NN: num_nets
 BS: batch_size
-ID: in_dim
-OD: out_dim
-LiID: layer_i_in_dim
-LiOD: layer_i_out_dim
+NI: num_inputs
+NO: num_outputs
+LiIS: layer_i_in_size
+LiOS: layer_i_out_size
 """
 
 import torch
@@ -17,21 +17,21 @@ class FeedforwardStaticNets(BaseStaticNets):
 
     def __init__(self, config: StaticNetsConfig):
         self.config: StaticNetsConfig = config
-        layer_dims: list[int] = [config.num_inputs] + config.hidden_layer_sizes + [config.num_outputs]
-        self.weights: list[Float[Tensor, "NN LiID LiOD"]] = []
-        self.biases: list[Float[Tensor, "NN 1 LiOD"]] = []
-        self.num_layers: int = len(layer_dims) - 1
+        layer_sizes: list[int] = [config.num_inputs] + config.hidden_layer_sizes + [config.num_outputs]
+        self.weights: list[Float[Tensor, "NN LiIS LiOS"]] = []
+        self.biases: list[Float[Tensor, "NN 1 LiOS"]] = []
+        self.num_layers: int = len(layer_sizes) - 1
         for j in range(self.num_layers):
-            layer_j_in_dim: int = layer_dims[j]
-            layer_j_out_dim: int = layer_dims[j + 1]
-            layer_j_std: float = (1.0 / layer_j_in_dim) ** 0.5
-            layer_j_weight: Float[Tensor, "NN LiID LiOD"] = torch.randn(self.config.num_nets, layer_j_in_dim, layer_j_out_dim) * layer_j_std
-            layer_j_bias: Float[Tensor, "NN 1 LiOD"] = torch.randn(self.config.num_nets, 1, layer_j_out_dim) * layer_j_std
+            layer_j_in_size: int = layer_sizes[j]
+            layer_j_out_size: int = layer_sizes[j + 1]
+            layer_j_std: float = (1.0 / layer_j_in_size) ** 0.5
+            layer_j_weight: Float[Tensor, "NN LiIS LiOS"] = torch.randn(self.config.num_nets, layer_j_in_size, layer_j_out_size) * layer_j_std
+            layer_j_bias: Float[Tensor, "NN 1 LiOS"] = torch.randn(self.config.num_nets, 1, layer_j_out_size) * layer_j_std
             self.weights.append(layer_j_weight)
             self.biases.append(layer_j_bias)
         if self.config.sigma_sigma is not None:
-            self.weight_sigmas: list[Float[Tensor, "NN LiID LiOD"]] = []
-            self.bias_sigmas: list[Float[Tensor, "NN 1 LiOD"]] = []
+            self.weight_sigmas: list[Float[Tensor, "NN LiIS LiOS"]] = []
+            self.bias_sigmas: list[Float[Tensor, "NN 1 LiOS"]] = []
             for layer_j_weight, layer_j_bias in zip(self.weights, self.biases):
                 self.weight_sigmas.append(torch.full_like(layer_j_weight, self.config.sigma))
                 self.bias_sigmas.append(torch.full_like(layer_j_bias, self.config.sigma))
@@ -39,22 +39,22 @@ class FeedforwardStaticNets(BaseStaticNets):
     def mutate(self) -> None:
         for j in range(self.num_layers):
             if self.config.sigma_sigma is not None:
-                layer_j_xi: Float[Tensor, "NN LiID LiOD"] = torch.randn_like(self.weight_sigmas[j]) * self.config.sigma_sigma
-                self.weight_sigmas[j]: Float[Tensor, "NN LiID LiOD"] = self.weight_sigmas[j] * (1 + layer_j_xi)
-                layer_j_weight_sigma: Float[Tensor, "NN LiID LiOD"] = self.weight_sigmas[j]
-                layer_j_xi_bias: Float[Tensor, "NN 1 LiOD"] = torch.randn_like(self.bias_sigmas[j]) * self.config.sigma_sigma
-                self.bias_sigmas[j]: Float[Tensor, "NN 1 LiOD"] = self.bias_sigmas[j] * (1 + layer_j_xi_bias)
-                layer_j_bias_sigma: Float[Tensor, "NN 1 LiOD"] = self.bias_sigmas[j]
+                layer_j_xi: Float[Tensor, "NN LiIS LiOS"] = torch.randn_like(self.weight_sigmas[j]) * self.config.sigma_sigma
+                self.weight_sigmas[j]: Float[Tensor, "NN LiIS LiOS"] = self.weight_sigmas[j] * (1 + layer_j_xi)
+                layer_j_weight_sigma: Float[Tensor, "NN LiIS LiOS"] = self.weight_sigmas[j]
+                layer_j_xi_bias: Float[Tensor, "NN 1 LiOS"] = torch.randn_like(self.bias_sigmas[j]) * self.config.sigma_sigma
+                self.bias_sigmas[j]: Float[Tensor, "NN 1 LiOS"] = self.bias_sigmas[j] * (1 + layer_j_xi_bias)
+                layer_j_bias_sigma: Float[Tensor, "NN 1 LiOS"] = self.bias_sigmas[j]
             else:
                 layer_j_weight_sigma: float = self.config.sigma
                 layer_j_bias_sigma: float = self.config.sigma
-            self.weights[j]: Float[Tensor, "NN LiID LiOD"] = self.weights[j] + torch.randn_like(self.weights[j]) * layer_j_weight_sigma
-            self.biases[j]: Float[Tensor, "NN 1 LiOD"] = self.biases[j] + torch.randn_like(self.biases[j]) * layer_j_bias_sigma
+            self.weights[j]: Float[Tensor, "NN LiIS LiOS"] = self.weights[j] + torch.randn_like(self.weights[j]) * layer_j_weight_sigma
+            self.biases[j]: Float[Tensor, "NN 1 LiOS"] = self.biases[j] + torch.randn_like(self.biases[j]) * layer_j_bias_sigma
 
-    def __call__(self, x: Float[Tensor, "NN BS ID"]) -> Float[Tensor, "NN BS OD"]:
+    def __call__(self, x: Float[Tensor, "NN BS NI"]) -> Float[Tensor, "NN BS NO"]:
         for j in range(self.num_layers):
-            x: Float[Tensor, "NN BS LiOD"] = torch.bmm(x, self.weights[j])
-            x: Float[Tensor, "NN BS LiOD"] = x + self.biases[j]
+            x: Float[Tensor, "NN BS LiOS"] = torch.bmm(x, self.weights[j])
+            x: Float[Tensor, "NN BS LiOS"] = x + self.biases[j]
             if j < self.num_layers - 1:
-                x: Float[Tensor, "NN BS LiOD"] = torch.tanh(x)
+                x: Float[Tensor, "NN BS LiOS"] = torch.tanh(x)
         return x
--- common/ne/pop/_nets/static/recurrent.py
+++ common/ne/pop/_nets/static/recurrent.py
@@ -2,10 +2,10 @@
 
 NN: num_nets
 BS: batch_size
-ID: in_dim
-OD: out_dim
-LiID: layer_i_in_dim
-LiOD: layer_i_out_dim
+NI: num_inputs
+NO: num_outputs
+LiIS: layer_i_in_size
+LiOS: layer_i_out_size
 """
 
 import torch
@@ -17,30 +17,30 @@ class RecurrentStaticNets(BaseStaticNets):
 
     def __init__(self, config: StaticNetsConfig):
         self.config: StaticNetsConfig = config
-        layer_dims: list[int] = [config.num_inputs] + config.hidden_layer_sizes + [config.num_outputs]
-        self.W_ih_weights: list[Float[Tensor, "NN LiOD LiID"]] = []
-        self.W_ih_biases: list[Float[Tensor, "NN 1 LiOD"]] = []
-        self.W_hh_weights: list[Float[Tensor, "NN LiOD LiOD"]] = []
-        self.W_hh_biases: list[Float[Tensor, "NN 1 LiOD"]] = []
-        self.num_layers: int = len(layer_dims) - 1
+        layer_sizes: list[int] = [config.num_inputs] + config.hidden_layer_sizes + [config.num_outputs]
+        self.W_ih_weights: list[Float[Tensor, "NN LiOS LiIS"]] = []
+        self.W_ih_biases: list[Float[Tensor, "NN 1 LiOS"]] = []
+        self.W_hh_weights: list[Float[Tensor, "NN LiOS LiOS"]] = []
+        self.W_hh_biases: list[Float[Tensor, "NN 1 LiOS"]] = []
+        self.num_layers: int = len(layer_sizes) - 1
         for j in range(self.num_layers):
-            layer_j_in_dim: int = layer_dims[j]
-            layer_j_out_dim: int = layer_dims[j + 1]
-            layer_j_ih_std: float = (1.0 / layer_j_in_dim) ** 0.5
-            layer_j_hh_std: float = (1.0 / layer_j_out_dim) ** 0.5
-            layer_j_W_ih: Float[Tensor, "NN LiOD LiID"] = torch.randn(self.config.num_nets, layer_j_out_dim, layer_j_in_dim) * layer_j_ih_std
-            layer_j_b_ih: Float[Tensor, "NN 1 LiOD"] = torch.randn(self.config.num_nets, 1, layer_j_out_dim) * layer_j_ih_std
-            layer_j_W_hh: Float[Tensor, "NN LiOD LiOD"] = torch.randn(self.config.num_nets, layer_j_out_dim, layer_j_out_dim) * layer_j_hh_std
-            layer_j_b_hh: Float[Tensor, "NN 1 LiOD"] = torch.randn(self.config.num_nets, 1, layer_j_out_dim) * layer_j_hh_std
+            layer_j_in_size: int = layer_sizes[j]
+            layer_j_out_size: int = layer_sizes[j + 1]
+            layer_j_ih_std: float = (1.0 / layer_j_in_size) ** 0.5
+            layer_j_hh_std: float = (1.0 / layer_j_out_size) ** 0.5
+            layer_j_W_ih: Float[Tensor, "NN LiOS LiIS"] = torch.randn(self.config.num_nets, layer_j_out_size, layer_j_in_size) * layer_j_ih_std
+            layer_j_b_ih: Float[Tensor, "NN 1 LiOS"] = torch.randn(self.config.num_nets, 1, layer_j_out_size) * layer_j_ih_std
+            layer_j_W_hh: Float[Tensor, "NN LiOS LiOS"] = torch.randn(self.config.num_nets, layer_j_out_size, layer_j_out_size) * layer_j_hh_std
+            layer_j_b_hh: Float[Tensor, "NN 1 LiOS"] = torch.randn(self.config.num_nets, 1, layer_j_out_size) * layer_j_hh_std
             self.W_ih_weights.append(layer_j_W_ih)
             self.W_ih_biases.append(layer_j_b_ih)
             self.W_hh_weights.append(layer_j_W_hh)
             self.W_hh_biases.append(layer_j_b_hh)
         if self.config.sigma_sigma is not None:
-            self.W_ih_weight_sigmas: list[Float[Tensor, "NN LiOD LiID"]] = []
-            self.W_ih_bias_sigmas: list[Float[Tensor, "NN 1 LiOD"]] = []
-            self.W_hh_weight_sigmas: list[Float[Tensor, "NN LiOD LiOD"]] = []
-            self.W_hh_bias_sigmas: list[Float[Tensor, "NN 1 LiOD"]] = []
+            self.W_ih_weight_sigmas: list[Float[Tensor, "NN LiOS LiIS"]] = []
+            self.W_ih_bias_sigmas: list[Float[Tensor, "NN 1 LiOS"]] = []
+            self.W_hh_weight_sigmas: list[Float[Tensor, "NN LiOS LiOS"]] = []
+            self.W_hh_bias_sigmas: list[Float[Tensor, "NN 1 LiOS"]] = []
             for layer_j_W_ih, layer_j_b_ih, layer_j_W_hh, layer_j_b_hh in zip(self.W_ih_weights, self.W_ih_biases, self.W_hh_weights, self.W_hh_biases):
                 self.W_ih_weight_sigmas.append(torch.full_like(layer_j_W_ih, self.config.sigma))
                 self.W_ih_bias_sigmas.append(torch.full_like(layer_j_b_ih, self.config.sigma))
@@ -50,42 +50,42 @@ class RecurrentStaticNets(BaseStaticNets):
     def mutate(self) -> None:
         for j in range(self.num_layers):
             if self.config.sigma_sigma is not None:
-                layer_j_xi_W_ih: Float[Tensor, "NN LiOD LiID"] = torch.randn_like(self.W_ih_weight_sigmas[j]) * self.config.sigma_sigma
-                self.W_ih_weight_sigmas[j]: Float[Tensor, "NN LiOD LiID"] = self.W_ih_weight_sigmas[j] * (1 + layer_j_xi_W_ih)
-                layer_j_W_ih_sigma: Float[Tensor, "NN LiOD LiID"] = self.W_ih_weight_sigmas[j]
-                layer_j_xi_b_ih: Float[Tensor, "NN 1 LiOD"] = torch.randn_like(self.W_ih_bias_sigmas[j]) * self.config.sigma_sigma
-                self.W_ih_bias_sigmas[j]: Float[Tensor, "NN 1 LiOD"] = self.W_ih_bias_sigmas[j] * (1 + layer_j_xi_b_ih)
-                layer_j_b_ih_sigma: Float[Tensor, "NN 1 LiOD"] = self.W_ih_bias_sigmas[j]
-                layer_j_xi_W_hh: Float[Tensor, "NN LiOD LiOD"] = torch.randn_like(self.W_hh_weight_sigmas[j]) * self.config.sigma_sigma
-                self.W_hh_weight_sigmas[j]: Float[Tensor, "NN LiOD LiOD"] = self.W_hh_weight_sigmas[j] * (1 + layer_j_xi_W_hh)
-                layer_j_W_hh_sigma: Float[Tensor, "NN LiOD LiOD"] = self.W_hh_weight_sigmas[j]
-                layer_j_xi_b_hh: Float[Tensor, "NN 1 LiOD"] = torch.randn_like(self.W_hh_bias_sigmas[j]) * self.config.sigma_sigma
-                self.W_hh_bias_sigmas[j]: Float[Tensor, "NN 1 LiOD"] = self.W_hh_bias_sigmas[j] * (1 + layer_j_xi_b_hh)
-                layer_j_b_hh_sigma: Float[Tensor, "NN 1 LiOD"] = self.W_hh_bias_sigmas[j]
+                layer_j_xi_W_ih: Float[Tensor, "NN LiOS LiIS"] = torch.randn_like(self.W_ih_weight_sigmas[j]) * self.config.sigma_sigma
+                self.W_ih_weight_sigmas[j]: Float[Tensor, "NN LiOS LiIS"] = self.W_ih_weight_sigmas[j] * (1 + layer_j_xi_W_ih)
+                layer_j_W_ih_sigma: Float[Tensor, "NN LiOS LiIS"] = self.W_ih_weight_sigmas[j]
+                layer_j_xi_b_ih: Float[Tensor, "NN 1 LiOS"] = torch.randn_like(self.W_ih_bias_sigmas[j]) * self.config.sigma_sigma
+                self.W_ih_bias_sigmas[j]: Float[Tensor, "NN 1 LiOS"] = self.W_ih_bias_sigmas[j] * (1 + layer_j_xi_b_ih)
+                layer_j_b_ih_sigma: Float[Tensor, "NN 1 LiOS"] = self.W_ih_bias_sigmas[j]
+                layer_j_xi_W_hh: Float[Tensor, "NN LiOS LiOS"] = torch.randn_like(self.W_hh_weight_sigmas[j]) * self.config.sigma_sigma
+                self.W_hh_weight_sigmas[j]: Float[Tensor, "NN LiOS LiOS"] = self.W_hh_weight_sigmas[j] * (1 + layer_j_xi_W_hh)
+                layer_j_W_hh_sigma: Float[Tensor, "NN LiOS LiOS"] = self.W_hh_weight_sigmas[j]
+                layer_j_xi_b_hh: Float[Tensor, "NN 1 LiOS"] = torch.randn_like(self.W_hh_bias_sigmas[j]) * self.config.sigma_sigma
+                self.W_hh_bias_sigmas[j]: Float[Tensor, "NN 1 LiOS"] = self.W_hh_bias_sigmas[j] * (1 + layer_j_xi_b_hh)
+                layer_j_b_hh_sigma: Float[Tensor, "NN 1 LiOS"] = self.W_hh_bias_sigmas[j]
             else:
                 layer_j_W_ih_sigma: float = self.config.sigma
                 layer_j_b_ih_sigma: float = self.config.sigma
                 layer_j_W_hh_sigma: float = self.config.sigma
                 layer_j_b_hh_sigma: float = self.config.sigma
-            self.W_ih_weights[j]: Float[Tensor, "NN LiOD LiID"] = self.W_ih_weights[j] + torch.randn_like(self.W_ih_weights[j]) * layer_j_W_ih_sigma
-            self.W_ih_biases[j]: Float[Tensor, "NN 1 LiOD"] = self.W_ih_biases[j] + torch.randn_like(self.W_ih_biases[j]) * layer_j_b_ih_sigma
-            self.W_hh_weights[j]: Float[Tensor, "NN LiOD LiOD"] = self.W_hh_weights[j] + torch.randn_like(self.W_hh_weights[j]) * layer_j_W_hh_sigma
-            self.W_hh_biases[j]: Float[Tensor, "NN 1 LiOD"] = self.W_hh_biases[j] + torch.randn_like(self.W_hh_biases[j]) * layer_j_b_hh_sigma
+            self.W_ih_weights[j]: Float[Tensor, "NN LiOS LiIS"] = self.W_ih_weights[j] + torch.randn_like(self.W_ih_weights[j]) * layer_j_W_ih_sigma
+            self.W_ih_biases[j]: Float[Tensor, "NN 1 LiOS"] = self.W_ih_biases[j] + torch.randn_like(self.W_ih_biases[j]) * layer_j_b_ih_sigma
+            self.W_hh_weights[j]: Float[Tensor, "NN LiOS LiOS"] = self.W_hh_weights[j] + torch.randn_like(self.W_hh_weights[j]) * layer_j_W_hh_sigma
+            self.W_hh_biases[j]: Float[Tensor, "NN 1 LiOS"] = self.W_hh_biases[j] + torch.randn_like(self.W_hh_biases[j]) * layer_j_b_hh_sigma
 
-    def __call__(self, x: Float[Tensor, "NN BS ID"]) -> Float[Tensor, "NN BS OD"]:
-        layer_dims: list[int] = [self.config.num_inputs] + self.config.hidden_layer_sizes + [self.config.num_outputs]
-        hidden_states: list[Float[Tensor, "NN BS LiOD"]] = []
+    def __call__(self, x: Float[Tensor, "NN BS NI"]) -> Float[Tensor, "NN BS NO"]:
+        layer_sizes: list[int] = [self.config.num_inputs] + self.config.hidden_layer_sizes + [self.config.num_outputs]
+        hidden_states: list[Float[Tensor, "NN BS LiOS"]] = []
         for j in range(self.num_layers):
-            layer_j_out_dim: int = layer_dims[j + 1]
-            layer_j_h: Float[Tensor, "NN BS LiOD"] = torch.zeros(self.config.num_nets, x.shape[1], layer_j_out_dim)
+            layer_j_out_size: int = layer_sizes[j + 1]
+            layer_j_h: Float[Tensor, "NN BS LiOS"] = torch.zeros(self.config.num_nets, x.shape[1], layer_j_out_size)
             hidden_states.append(layer_j_h)
         layer_input: Float[Tensor, "NN BS dim"] = x
         for j in range(self.num_layers):
-            layer_j_ih: Float[Tensor, "NN BS LiOD"] = torch.bmm(layer_input, self.W_ih_weights[j].transpose(-1, -2))
-            layer_j_ih: Float[Tensor, "NN BS LiOD"] = layer_j_ih + self.W_ih_biases[j]
-            layer_j_hh: Float[Tensor, "NN BS LiOD"] = torch.bmm(hidden_states[j], self.W_hh_weights[j].transpose(-1, -2))
-            layer_j_hh: Float[Tensor, "NN BS LiOD"] = layer_j_hh + self.W_hh_biases[j]
-            layer_j_h_new: Float[Tensor, "NN BS LiOD"] = torch.tanh(layer_j_ih + layer_j_hh)
+            layer_j_ih: Float[Tensor, "NN BS LiOS"] = torch.bmm(layer_input, self.W_ih_weights[j].transpose(-1, -2))
+            layer_j_ih: Float[Tensor, "NN BS LiOS"] = layer_j_ih + self.W_ih_biases[j]
+            layer_j_hh: Float[Tensor, "NN BS LiOS"] = torch.bmm(hidden_states[j], self.W_hh_weights[j].transpose(-1, -2))
+            layer_j_hh: Float[Tensor, "NN BS LiOS"] = layer_j_hh + self.W_hh_biases[j]
+            layer_j_h_new: Float[Tensor, "NN BS LiOS"] = torch.tanh(layer_j_ih + layer_j_hh)
             hidden_states[j] = layer_j_h_new
             layer_input = layer_j_h_new
         return hidden_states[-1]
